b0922b1bf0515d67fa63824c8e5479e6a072368d
STIg89 <andreystig89@gmail.com>
2023-11-24 22:22:47 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html'],
+  content: ['./src/*.{html,js}', './src/partials/*.html'],
   theme: {
     screens: {
       sm: '480px',
-      // => @media (min-width: 480px) { ... }
 
       md: '768px',
-      // => @media (min-width: 768px) { ... }
 
       lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
 
       xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
 
       '3xl': '1920px',
-      // => @media (min-width: 1920px) { ... }
     },
     colors: {
       beige: '#F1DFC3',
-      green: '#DFB53B',
+      green: '#727D57',
       greenBTN: '#357A38',
       yellow: '#DFB53B',
       black: '#DAD8D5',
