107a8eb1db049eda8e2364679b4c6686f8e1b9d1
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2023-11-29 22:48:16 +0000

    Deploying to gh-pages from @ baza-trainee/zerno-social@02d7d086cdee001ffb0bba68cde9e658c66adbb5 🚀

---
+++ index.007f4433.js
@@ -0,0 +1,2 @@
+(()=>{const e={toggleMenuBtn:document.querySelector("[data-menu-toggler]"),modal:document.querySelector("[data-menu]")};e.toggleMenuBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.007f4433.js.map
---
+++ index.56e75521.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={toggleMenuBtn:document.querySelector("[data-menu-toggler]"),modal:document.querySelector("[data-menu]")}).toggleMenuBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.56e75521.js.map
