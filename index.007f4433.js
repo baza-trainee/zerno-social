cf80590b963dbc8e0934532db45c47b04c563e41
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2023-12-02 17:34:57 +0000

    Deploying to gh-pages from @ baza-trainee/zerno-social@bf453776810f844b87f21844a4ff135fa8510268 🚀

--- index.007f4433.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={toggleMenuBtn:document.querySelector("[data-menu-toggler]"),modal:document.querySelector("[data-menu]")};e.toggleMenuBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
-//# sourceMappingURL=index.007f4433.js.map
