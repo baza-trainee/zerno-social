64c945d668930775409d82ef644f936331499dc3
iryna-zatynina <ira.zat1997@gmail.com>
2023-12-02 12:47:15 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './js/menu';
-import Swiper from 'swiper';
-
-var directionsSwiper = new Swiper(".directionsSwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
---
+++ src/js/swipers.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+
+var directionsSwiper = new Swiper(".directionsSwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
