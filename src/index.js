dafc0e402261dbfcc109efad3894c46578dffef9
iryna-zatynina <ira.zat1997@gmail.com>
2023-12-02 11:28:56 +0100

    directions code review fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './js/menu';
+import Swiper from 'swiper';
+
+var directionsSwiper = new Swiper(".directionsSwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
