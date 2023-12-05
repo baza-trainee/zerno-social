8bbaf4652b80df529d63462c9bb95629e47ff527
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-12-05 20:22:26 +0200

    fix markup in faq footer contact

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([48.4312, 22.7172], 13);
+var map = L.map('map').setView([48.7902308, 22.9469080], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var marker = L.marker([48.4312, 22.7172]).addTo(map)
+var marker = L.marker([48.7902308, 22.9469080]).addTo(map)
     .bindPopup('<b>Село Збини 29</b><br>Мукачівський район<br>Закарпатська область<br>Україна.')
     .openPopup();
