dbc7d124745117bd7a3a84779a7bfe2618b388f3
STIg89 <andreystig89@gmail.com>
2023-12-08 14:25:58 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import './js/menu';
-<<<<<<< HEAD
-import './js/swipers';
-import './js/more'; 
-import './js/faq'; 
-
-=======
 import './js/faq';
 import './js/activities';
 import './js/map';
->>>>>>> b265e5665edfa6dd3a9fd352a05095d82e31e9b1
+import './js/more';
