cb3a3977f9f04063fb2bae3e5b33f48fbbdd76f3
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2023-12-15 01:07:18 +0200

    created a button to switch the language, partial translation added

---
+++ src/js/languageTranslate.js
@@ -0,0 +1,202 @@
+export default {
+  header_text_1: {
+    ua: 'c.Збини 29, Мукачівській р-н, Закарпатська обл.',
+    en: 'Zbyny 29, Mukachevo district, Transcarpathian region.',
+  },
+  header_text_2: {
+    ua: 'Про нас',
+    en: 'About us',
+  },
+  header_text_3: {
+    ua: 'Проєкти',
+    en: 'Projects',
+  },
+  header_text_4: {
+    ua: 'Контакти',
+    en: 'Contacts',
+  },
+  header_text_5: {
+    ua: 'Співпраця',
+    en: 'Collaboration',
+  },
+  header_nav_menu_1: {
+    ua: 'c.Збини 29, Мукачівській р-н, Закарпатська обл.',
+    en: 'Zbyny 29, Mukachevo district, Transcarpathian region.',
+  },
+  header_nav_menu_2: {
+    ua: 'Про нас',
+    en: 'About us',
+  },
+  header_nav_menu_3: {
+    ua: 'Проєкти',
+    en: 'Projects',
+  },
+  header_nav_menu_4: {
+    ua: 'Контакти',
+    en: 'Contacts',
+  },
+  header_nav_menu_5: {
+    ua: 'Співпраця',
+    en: 'Collaboration',
+  },
+  contribution_1: {
+    ua: 'Запрошуємо до співпраці',
+    en: 'We invite you to cooperate',
+  },
+  contribution_2: {
+    ua: 'Волонтери',
+    en: 'Volunteers',
+  },
+  contribution_3: {
+    ua: 'Якщо Ви відчуваєте потребу поділитися своїми знаннями, навичками італантами - запрошуємо до команди! Разом ми зробимо більше',
+    en: 'If you feel the need to share your knowledge, skills and talents, we invite you to join our team! Together we will do more',
+  },
+  contribution_4: {
+    ua: 'Дізнатись більше',
+    en: 'Learn more',
+  },
+  contribution_5: {
+    ua: 'Ментори',
+    en: 'Mentors',
+  },
+  contribution_6: {
+    ua: 'Ви можете записати надихайюче відео для сільських дітей абоприїхати і зустрітися з людьми, які будуть вдячні почути Вас і надихнутися Вашим досвідом та досягненнями!',
+    en: 'You can record an inspirational video for rural children or come and meet people who will be grateful to hear from you and inspired by your experience and achievements!',
+  },
+  contribution_7: {
+    ua: 'Дізнатись більше',
+    en: 'Learn more',
+  },
+  contribution_8: {
+    ua: 'Партнери',
+    en: 'Partners',
+  },
+  contribution_9: {
+    ua: 'Станьте партнерами ГО ЗЕРНО і долучіться до розвитку щасливих сільських громад! Ви можете долучитися своїм досвідом,товаром, якій виробляєте, або ж матеріальною підтримкою актуальних проєктів! Станьте партнерами ГО ЗЕРНО і долучіться до розвитку',
+    en: 'Become a partner of ZERNO and contribute to the development of happy rural communities! You can contribute with your experience, products, or material support for current projects! Become a partner of the NGO ZERNO and join the development',
+  },
+  contribution_10: {
+    ua: 'Дізнатись більше',
+    en: 'Learn more',
+  },
+  mentors_1: {
+    ua: 'Що може входити в обов’язки ментора',
+    en: "What may be included in the mentor's responsibilities",
+  },
+  mentors_2: {
+    ua: 'Проводити семінари',
+    en: 'Conduct seminars',
+  },
+  mentors_3: {
+    ua: 'Давати наставництво',
+    en: 'Provide mentoring',
+  },
+  mentors_4: {
+    ua: 'Розвивати лідерські навички учасників',
+    en: 'Develop leadership skills of participants',
+  },
+  mentors_5: {
+    ua: 'Вирішення підліткових питань',
+    en: 'Solving teenage issues',
+  },
+  mentors_6: {
+    ua: 'Надавати корисні матеріали',
+    en: 'Provide useful materials',
+  },
+  mentors_7: {
+    ua: 'Консультувати',
+    en: 'Consult',
+  },
+  mentors_8: {
+    ua: 'Ментор',
+    en: 'Mentor',
+  },
+  mentors_9: {
+    ua: '- це досвідчена людина, яка надає підтримку, поради та наставництво менш досвідченим людям, зазвичай в межах професійного або особистісного розвитку. Ментор допомагає розвивати навички, вдосконалювати знання та досягати поставлених цілей. Нам дуже потрібні експерти в різних сферах для розвитку молоді, дітей та жінок.',
+    en: '- is an experienced person who provides support, advice, and mentoring to less experienced people, usually as part of a professional or personal development development. A mentor helps to develop skills, improve knowledge, and achieve goals. We really need experts in various fields for the development of youth, children and women.',
+  },
+  mentors_10: {
+    ua: 'Закрити',
+    en: 'Close',
+  },
+  partners_1: {
+    ua: 'Що може входити в обов’язки партнера',
+    en: "What may be the partner's responsibilities",
+  },
+  partners_2: {
+    ua: 'Фінансова підтримка',
+    en: 'Financial support',
+  },
+  partners_3: {
+    ua: 'Матеріальна підтримка',
+    en: 'Material support',
+  },
+  partners_4: {
+    ua: 'Підтримка загального добробуту',
+    en: 'Supporting the general welfare',
+  },
+  partners_5: {
+    ua: 'Підтримка культурного розвитку',
+    en: 'Support for cultural development',
+  },
+  partners_6: {
+    ua: 'Облаштування приміщення',
+    en: 'Room arrangement',
+  },
+  partners_7: {
+    ua: 'Фінансування поточних витрат',
+    en: 'Financing of current expenses',
+  },
+  partners_8: {
+    ua: 'Партнери',
+    en: 'Partners',
+  },
+  partners_9: {
+    ua: "- це спонсори та меценати або організація які надають фінансову чи матеріальну підтримку для реалізації проектів подій чи діяльності спільноти. Партнери надають фінансову підтримку в обмін на рекламу чи інші вигоди, що пов'язані з подією чи проєктом або без прямої вигоди вкладають внесок у розвиток організації.",
+    en: '- are sponsors and patrons or organizations that provide financial or or material support for the realization of events or community activities. community projects. Partners provide financial support in exchange for advertising or other benefits related to the event or project or without direct benefit contribute to the development of the organization.',
+  },
+  partners_10: {
+    ua: 'Закрити',
+    en: 'Close',
+  },
+  volonteer_1: {
+    ua: 'Що може входити у обов’язки волонтера',
+    en: "What can be included in the volunteer's duties",
+  },
+  volonteer_2: {
+    ua: 'Створення контенту',
+    en: 'Content creation',
+  },
+  volonteer_3: {
+    ua: 'Вести соцмережі',
+    en: 'Maintain social networks',
+  },
+  volonteer_4: {
+    ua: 'Організаційні питання',
+    en: 'Organizational issues',
+  },
+  volonteer_5: {
+    ua: 'Робота руками',
+    en: 'Work with your hands',
+  },
+  volonteer_6: {
+    ua: 'Комунікація з учасниками',
+    en: 'Communication with participants',
+  },
+  volonteer_7: {
+    ua: 'Листування',
+    en: 'Correspondence',
+  },
+  volonteer_8: {
+    ua: 'Волонтер',
+    en: 'Volunteer',
+  },
+  volonteer_9: {
+    ua: '- це добровільна участь у роботі або прєкті на безоплатній основі з метою допомогти та підтримки спільноти та її ідеї. Якщо ви хочете стати волонтером та у вас є ідеї для співпраці, то негайно заповнюйте форму для співпраці, а наш менеджер протягом дня вам зателефонує. Ми цінуємо ваше бажання долучитися до нашого проєкту та раді вислухати будь-які пропозиції.',
+    en: '- is voluntary participation in a work or project on a pro bono basis to help and support the community and its ideas. If you want to become a a volunteer and you have ideas for cooperation, then immediately fill out the for cooperation, and our manager will contact you within a day. We appreciate your desire to join our project and are happy to listen to any suggestions.',
+  },
+  volonteer_10: {
+    ua: 'Закрити',
+    en: 'Close',
+  },
+};
--- src/js/switchLangBtn.js
+++ src/js/switchLangBtn.js
@@ -1,39 +1,92 @@
-(() => {
-  const refs = {
-    switchLang: document.querySelector('[switch-lang-btn]'),
-    notSwitchedLang: document.querySelector('[not-switched-lang-btn]'),
-    switchUA: document.querySelector('[switch-lang-ua]'),
-    switchEN: document.querySelector('[switch-lang-en]'),
-    currentUA: document.querySelector('[current-lang-ua]'),
-    currentEN: document.querySelector('[current-lang-en]'),
-  };
+import languageTranslate from './languageTranslate';
 
-  refs.switchLang.addEventListener('click', toggleLang);
-  refs.switchUA.addEventListener('click', () => switchLanguage('ua'));
-  refs.switchEN.addEventListener('click', () => switchLanguage('en'));
+const arrayLanguage = ['UK', 'EN'];
 
-  function toggleLang() {
-    refs.notSwitchedLang.classList.toggle('hidden');
-  }
+let activeLanguage = localStorage.getItem('language') || 'UA';
+const languageOptions = document.querySelector('.language-options');
+const switchButton = document.querySelector('.language-dropdown button ');
 
-  function switchLanguage(language) {
-    const switchLangUA = refs.switchUA;
-    const switchLangEN = refs.switchEN;
-    const currentUA = refs.currentUA;
-    const currentEN = refs.currentEN;
+function toggleLanguageOptions() {
+  languageOptions.classList.toggle('hidden');
+  switchButton.disabled = true;
+  switchButton.style.opacity = '0.5';
+}
 
-    if (language === 'ua') {
-      switchLangUA.classList.add('hidden');
-      switchLangEN.classList.remove('hidden');
-      currentEN.classList.add('hidden');
-      currentUA.classList.remove('hidden');
-    } else {
-      switchLangUA.classList.remove('hidden');
-      switchLangEN.classList.add('hidden');
-      currentEN.classList.remove('hidden');
-      currentUA.classList.add('hidden');
+function switchLanguage(lang) {
+  const activeButton = document.getElementById(
+    `${activeLanguage.toLowerCase()}Btn`
+  );
+  const newButton = document.getElementById(`${lang.toLowerCase()}Btn`);
+
+  activeButton.classList.remove('hidden');
+  newButton.classList.add('hidden');
+
+  const switchButtonSpan = document.querySelector(
+    '.language-dropdown  button  span '
+  );
+  switchButtonSpan.innerHTML = `${lang}`;
+
+  localStorage.setItem('language', lang);
+
+  toggleLanguageOptions();
+  activeLanguage = lang;
+  switchButton.disabled = false;
+  switchButton.style.opacity = '1';
+
+  setTimeout(() => {
+    window.location.reload();
+  }, 300);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.language-dropdown  button  span ').innerHTML =
+    activeLanguage;
+
+  const listBtnOperatuons = document.querySelectorAll(
+    '.language-options button'
+  );
+
+  listBtnOperatuons.forEach(btn => {
+    if (btn.classList.contains('hidden')) {
+      btn.remove('hidden');
     }
+  });
 
-    toggleLang();
+  function updateLanguageLink(languageCode) {
+    const currentHash = window.location.hash;
+    const newPath = `/${languageCode.toLowerCase()}${currentHash}`;
+    window.history.replaceState({}, document.title, newPath);
   }
-})();
+
+  const newButton = document.getElementById(
+    `${activeLanguage.toLowerCase()}Btn`
+  );
+
+  newButton.classList.add('hidden');
+
+  const uaButton = document.getElementById('uaBtn');
+  const enButton = document.getElementById('enBtn');
+
+  switchButton.addEventListener('click', toggleLanguageOptions);
+
+  uaButton.addEventListener('click', function () {
+    switchLanguage('UA');
+    updateLanguageLink('UA');
+  });
+
+  enButton.addEventListener('click', function () {
+    switchLanguage('EN');
+    updateLanguageLink('EN');
+  });
+
+  updateLanguageLink(activeLanguage);
+});
+
+const chengeLang = () => {
+  for (const key in languageTranslate) {
+    const elem = document.querySelector(`[data-lang=${key}]`);
+    elem.textContent = languageTranslate[key][activeLanguage.toLowerCase()];
+  }
+};
+
+chengeLang();
