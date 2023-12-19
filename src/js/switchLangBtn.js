e07287a9a71f7a6afab956cd76b08b7eae567f2e
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2023-12-20 00:36:40 +0200

    added the ability to cancel the language selection by clicking anywhere

--- src/js/switchLangBtn.js
+++ src/js/switchLangBtn.js
@@ -3,11 +3,36 @@ import languageTranslate from './languageTranslate';
 let activeLanguage = localStorage.getItem('language') || 'UA';
 const languageOptions = document.querySelector('.language-options');
 const switchButton = document.querySelector('.language-dropdown button ');
+const body = document.querySelector('body');
+let isLanguageOptionsVisible = false;
+
+function checkMousedown(e) {
+  if (
+    isLanguageOptionsVisible &&
+    !languageOptions.contains(e.target) &&
+    !Array.from(languageOptions.querySelectorAll('*')).includes(e.target)
+  ) {
+    switchButton.disabled = false;
+    switchButton.style.opacity = '1';
+    isLanguageOptionsVisible = false;
+    languageOptions.classList.add('hidden');
+    body.removeEventListener('mousedown', checkMousedown);
+  }
+}
 
 function toggleLanguageOptions() {
+  if (!languageOptions.classList.contains('hidden')) {
+    switchButton.disabled = false;
+    switchButton.style.opacity = '1';
+    isLanguageOptionsVisible = false;
+  } else {
+    switchButton.disabled = true;
+    switchButton.style.opacity = '0.5';
+    isLanguageOptionsVisible = true;
+  }
   languageOptions.classList.toggle('hidden');
-  switchButton.disabled = true;
-  switchButton.style.opacity = '0.5';
+
+  body.addEventListener('mousedown', checkMousedown);
 }
 
 function switchLanguage(lang) {
@@ -28,8 +53,7 @@ function switchLanguage(lang) {
 
   toggleLanguageOptions();
   activeLanguage = lang;
-  switchButton.disabled = false;
-  switchButton.style.opacity = '1';
+
   chengeLang();
 }
 
