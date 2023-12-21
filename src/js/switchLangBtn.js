13d1c47ccff7fe81aedeaf178a7efa1b1716191c
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2023-12-21 20:30:43 +0200

    added the ability to close the language selection window by clicking on the button of the already selected language

--- src/js/switchLangBtn.js
+++ src/js/switchLangBtn.js
@@ -1,38 +1,22 @@
 import languageTranslate from './languageTranslate';
 
 let activeLanguage = localStorage.getItem('language') || 'UA';
-const languageOptions = document.querySelector('.language-options');
-const switchButton = document.querySelector('.language-dropdown button ');
-const body = document.querySelector('body');
-let isLanguageOptionsVisible = false;
 
-function checkMousedown(e) {
-  if (
-    isLanguageOptionsVisible &&
-    !languageOptions.contains(e.target) &&
-    !Array.from(languageOptions.querySelectorAll('*')).includes(e.target)
-  ) {
-    switchButton.disabled = false;
-    switchButton.style.opacity = '1';
-    isLanguageOptionsVisible = false;
-    languageOptions.classList.add('hidden');
-    body.removeEventListener('mousedown', checkMousedown);
-  }
-}
+const languageOptions = document.querySelector('.language-options');
+const listBtnOperatuons = document.querySelectorAll('.language-options button');
+const switchButton = document.querySelector('.language-dropdown button ');
+const switchButtonSpan = document.querySelector(
+  '.language-dropdown  button  span '
+);
 
 function toggleLanguageOptions() {
-  if (!languageOptions.classList.contains('hidden')) {
-    switchButton.disabled = false;
-    switchButton.style.opacity = '1';
-    isLanguageOptionsVisible = false;
-  } else {
-    switchButton.disabled = true;
-    switchButton.style.opacity = '0.5';
-    isLanguageOptionsVisible = true;
-  }
   languageOptions.classList.toggle('hidden');
 
-  body.addEventListener('mousedown', checkMousedown);
+  if (languageOptions.classList.contains('hidden')) {
+    switchButton.style.opacity = '1';
+  } else {
+    switchButton.style.opacity = '0.5';
+  }
 }
 
 function switchLanguage(lang) {
@@ -44,26 +28,19 @@ function switchLanguage(lang) {
   activeButton.classList.remove('hidden');
   newButton.classList.add('hidden');
 
-  const switchButtonSpan = document.querySelector(
-    '.language-dropdown  button  span '
-  );
   switchButtonSpan.innerHTML = `${lang}`;
 
   localStorage.setItem('language', lang);
 
   toggleLanguageOptions();
   activeLanguage = lang;
-
+  switchButton.style.opacity = '1';
   chengeLang();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.language-dropdown  button  span ').innerHTML =
     activeLanguage;
-
-  const listBtnOperatuons = document.querySelectorAll(
-    '.language-options button'
-  );
 
   listBtnOperatuons.forEach(btn => {
     if (btn.classList.contains('hidden')) {
