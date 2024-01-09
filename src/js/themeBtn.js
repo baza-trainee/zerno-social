9568a769a4915ef0f655c9de854988bdc9dc8dbd
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2024-01-10 00:43:41 +0200

    fixed theme switching

--- src/js/themeBtn.js
+++ src/js/themeBtn.js
@@ -2,10 +2,16 @@ const btnTheme = document.querySelector("[data-theme='btn']");
 const elementTheme = document.querySelector("[data-theme='element']");
 
 window.addEventListener('DOMContentLoaded', () => {
-
-    console.log(window.matchMedia('(prefers-color-scheme: dark)'))
   const storedTheme = localStorage.getItem('theme');
-  
+
+  if (storedTheme === null) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      return;
+    }
+  }
+
   if (storedTheme === 'dark') {
     document.documentElement.classList.add('dark');
     localStorage.setItem('theme', 'dark');
@@ -14,16 +20,13 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-btnTheme.addEventListener('click', function(){
-    const theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-    } else {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    }
+btnTheme.addEventListener('click', function () {
+  const theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  }
 });
-
-
-
