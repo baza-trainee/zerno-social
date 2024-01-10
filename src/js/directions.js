a95cf42ed2be1b0036840ed07cfd5205fbd45666
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2024-01-10 23:20:54 +0200

    fixed incorrect display of the item in the carousel

--- src/js/directions.js
+++ src/js/directions.js
@@ -1,8 +1,47 @@
 var directionsSwiper = new Swiper('.directionsSwiper', {
   slidesPerView: 'auto',
-  spaceBetween: 30,
+  spaceBetween: 40,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 60,
+    },
+    350:{
+      spaceBetween: 15,
+    },
+    360:{
+      spaceBetween: 16,
+    },
+    370:{
+      spaceBetween: 22,
+    },
+    380:{
+      spaceBetween: 25,
+    },
+    390:{
+      spaceBetween: 30,
+    },
+
+    400:{
+      spaceBetween:35,
+    },
+    420:{
+      spaceBetween:40,
+    },
+    435:{
+      spaceBetween:45,
+    },
+    445:{
+      spaceBetween:50,
+    },
+    460:{
+      spaceBetween:60,
+    },
+    480:{
+      spaceBetween:17,
+    }
   },
 });
